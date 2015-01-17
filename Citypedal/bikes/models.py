7cf010ccc44cd629fa526cf8f829745bc8bd0b14
Krzysztof Socha <ksocha@ksocha.com>
2015-01-17 20:29:49 +0100

    pep8'd

--- Citypedal/bikes/models.py
+++ Citypedal/bikes/models.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from decimal import Decimal
 from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
- 
+
     def _create_user(self, username, email, password, level, **extra_fields):
         # TODO popraw integrityerrory pls
         now = timezone.now()
@@ -16,19 +17,19 @@ class UserManager(BaseUserManager):
             raise ValueError('The given username must be set')
         email = self.normalize_email(email)
         user = self.model(username=username, email=email,
-             level=level, last_login=now,
-             joined_at=now, **extra_fields)
+                          level=level, last_login=now,
+                          joined_at=now, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
- 
+
     def create_user(self, username, email=None, password=None, **extra_fields):
-        return self._create_user(username, email, password, self.model.LEVEL_USER,
-            **extra_fields)
- 
+        return self._create_user(username, email, password,
+                                 self.model.LEVEL_USER, **extra_fields)
+
     def create_superuser(self, username, email, password, **extra_fields):
-        user=self._create_user(username, email, password, self.model.LEVEL_ADMIN,
-            **extra_fields)
+        user = self._create_user(username, email, password,
+                                 self.model.LEVEL_ADMIN, **extra_fields)
         user.save(using=self._db)
         return user
 
@@ -40,8 +41,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
     joined_at = models.DateTimeField(auto_now_add=True)
-    balance = models.DecimalField(max_digits=7,
-        decimal_places=2, default=Decimal(0))
+    balance = models.DecimalField(max_digits=7, decimal_places=2,
+                                  default=Decimal(0))
 
     LEVEL_USER = 1
     LEVEL_SERVICE = 3
@@ -54,15 +55,17 @@ class User(AbstractBaseUser, PermissionsMixin):
         (LEVEL_ADMIN, "Gandalf")
     )
     level = models.PositiveSmallIntegerField(choices=LEVEL_CHOICES,
-        default=LEVEL_USER)
+                                             default=LEVEL_USER)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
 
     objects = UserManager()
 
+
 class Transaction(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="transactions")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             related_name="transactions")
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     trip = models.ForeignKey("Trip", null=True, blank=True)
 
@@ -91,9 +94,9 @@ class Bike(models.Model):
         (STATE_BORROWED, "Wypożyczony"),
     )
     state = models.CharField(max_length=1, choices=STATE_CHOICES,
-        default=STATE_AVAILABLE)
+                             default=STATE_AVAILABLE)
     station = models.ForeignKey('Station', related_name="bikes",
-        null=True, blank=True)
+                                null=True, blank=True)
 
 
 class Station(models.Model):
@@ -111,7 +114,7 @@ class Service(models.Model):
 class Trip(models.Model):
     from_station = models.ForeignKey(Station, related_name="beginning_trips")
     to_station = models.ForeignKey(Station, related_name="ending_trips",
-        null=True, blank=True)
+                                   null=True, blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="trips")
     bike = models.ForeignKey(Bike, related_name="trips")
     started_at = models.DateTimeField(auto_now_add=True)
@@ -137,4 +140,5 @@ class Trip(models.Model):
             self.ended_at = datetime.now()
         super().save()
         Transaction.objects.create(user=self.user,
-            amount=self.price, trip=self, type=Transaction.TYPE_TRIP)
+                                   amount=self.price,
+                                   trip=self, type=Transaction.TYPE_TRIP)
